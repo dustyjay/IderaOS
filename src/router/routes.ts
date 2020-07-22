832be069dd54b6a87d3d2f3463bedf318e1b047a
janearisah <janet@chaka.ng>
2020-07-22 02:59:04 +0100

    added logout route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,6 +3,8 @@ import { RouteConfig } from 'vue-router'
 const Home = () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
 const Login = () =>
   import(/* webpackChunkName: "login" */ '../views/auth/Login.vue')
+const Logout = () =>
+  import(/* webpackChunkName: "logout" */ '../views/auth/Logout.vue')
 const Register = () =>
   import(/* webpackChunkName: "register" */ '../views/auth/Register.vue')
 const DashboardIndex = () =>
@@ -20,6 +22,11 @@ const routes: Array<RouteConfig> = [
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    component: Logout
   },
   {
     path: '/register',
--- src/views/auth/Logout.vue
+++ src/views/auth/Logout.vue
@@ -9,7 +9,7 @@ export default {
   name: 'Logout',
   mounted() {
     setTimeout(() => {
-      this.$router.push({ name: 'login' })
+      this.$router.push({ name: 'home' })
     }, 2000)
   }
 }
